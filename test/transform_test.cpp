2934b13cbc0d13f006db8393e3446c38b7d0e692
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-05-21 20:46:40 +0530

    added pipe operator test

--- test/transform_test.cpp
+++ test/transform_test.cpp
@@ -13,3 +13,16 @@ TEST_CASE("working parser mapping") {
                                              | parser::transform(to_dig);
   static_assert(dig_parser("123") == std::pair{ 1, "23"sv });
 };
+
+TEST_CASE("working parser mapping") {
+  constexpr parser::Parser auto dig_parser = parser::one_of("0123456789")//
+                                             | parser::transform(to_dig);
+  static_assert(dig_parser("123") == std::pair{ 1, "23"sv });
+};
+
+TEST_CASE("pipe operator test") {
+  constexpr parser::Parser auto dig_parser =
+    parser::always(2)//
+    | parser::transform([](auto) { return 3; });
+  static_assert(dig_parser("123") == std::pair{ 3, "123"sv });
+};
